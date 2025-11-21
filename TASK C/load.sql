17cd8f8f3126f49a5c35857d08bc07ac75e613a3
Pushyboy <jwong47@u.rochester.edu>
2025-11-21 00:02:09 -0500

    Changed load.sql to use autoincrement instead of csv ids

--- TASK C/load.sql
+++ TASK C/load.sql
@@ -3,7 +3,7 @@ INTO TABLE department
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
-(id, name, building);
+(name, building);
 
 LOAD DATA LOCAL INFILE './data/doctor_data.csv'
 INTO TABLE doctor
@@ -11,7 +11,7 @@ FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
-(id, password, first_name, last_name, phone, email, department_id, is_active);
+(password, first_name, last_name, phone, email, department_id, is_active);
 
 LOAD DATA LOCAL INFILE './data/address_info_data.csv'
 INTO TABLE address_info
@@ -26,7 +26,7 @@ FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
-(id, password,first_name,last_name,dob,phone,email,address_street,
+(password,first_name,last_name,dob,phone,email,address_street,
 address_country,address_zip,insurance,pharmacy_address);
 
 LOAD DATA LOCAL INFILE './data/appointment_data.csv'
@@ -35,7 +35,7 @@ FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
-(id, start, end, notes, patient_id, doctor_id);
+(start, end, notes, patient_id, doctor_id);
 
 LOAD DATA LOCAL INFILE './data/prescription_data.csv'
 INTO TABLE prescription
@@ -43,4 +43,4 @@ FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
-(id, name, dosage, expiration, appointment_id);
+(name, dosage, expiration, appointment_id);
