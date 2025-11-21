cc13c580c235916d9b21c7955a6b89a23f56ef03
MuratAl49 <muratal.engr@gmail.com>
2025-11-21 16:32:46 -0500

    load.sql recovered

--- TASK C/load.sql
+++ TASK C/load.sql
@@ -1,20 +1,27 @@
-LOAD DATA LOCAL INFILE './data/appointment_data.csv'
-INTO TABLE Appointment
+LOAD DATA LOCAL INFILE './data/department_data.csv'
+INTO TABLE department
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(name, building);
+
+LOAD DATA LOCAL INFILE './data/doctor_data.csv'
+INTO TABLE doctor
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
-IGNORE 1 ROWS -- skip CSV header
-(id, start, end, notes, patient_id, doctor_id);
+IGNORE 1 ROWS
+(password, first_name, last_name, phone, email, department_id, is_active);
 
 LOAD DATA LOCAL INFILE './data/address_info_data.csv'
-INTO TABLE Address_info
+INTO TABLE address_info
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (address_country, address_zip, address_state);
 
-LOAD DATA LOCAL INFILE './data/patient_data.csv'
-INTO TABLE Patients
+LOAD DATA LOCAL INFILE './data/patients_data.csv'
+INTO TABLE patient
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
@@ -22,4 +29,18 @@ IGNORE 1 ROWS
 (password,first_name,last_name,dob,phone,email,address_street,
 address_country,address_zip,insurance,pharmacy_address);
 
+LOAD DATA LOCAL INFILE './data/appointment_data.csv'
+INTO TABLE appointment
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(start, end, notes, patient_id, doctor_id);
 
+LOAD DATA LOCAL INFILE './data/prescription_data.csv'
+INTO TABLE prescription
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(name, dosage, expiration, appointment_id);
