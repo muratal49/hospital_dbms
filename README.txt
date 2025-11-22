a5c03786b1c73d552fc4505c1db3209deae0d998
Pushyboy <jwong47@u.rochester.edu>
2025-11-21 22:20:44 -0500

    Added patient dashboard

--- README.txt
+++ README.txt
@@ -1,2 +1,15 @@
 - Update Task A to reflect changes in relations (addresses)
 - Link everything together
+
+
+User:
+After submission, redirect back to dashboard
+
+Schedule Appointment => schedule.php
+View Appointment => patient_view_appointments.php
+Update Information => patient
+
+
+Caregiver:
+Patient Check In => caregiver_patient_checkin.php
+
---
+++ TASK_B/patient_dashboard.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["patient_id"])) {
+  header('Location: login.php');
+  exit();
+}
+
+if (isset($_POST["schedule_apt_btn"])) {
+  header('header: schedule.php');
+  exit();
+} else if (isset($_POST["view_apt_btn"])) {
+  header('header: patient_view_appointments.php');
+  exit();
+} else if (isset($_POST["update_info_btn"])) {
+  header('header: update_information.php');
+  exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Patient Dashboard</title>
+  <link rel="stylesheet" href="styles.css" />
+</head>
+
+<body>
+  <h1 class="header">Patient Dashboard</h1>
+
+  <div class="form_container">
+    <form class="/submit" method="post">
+      <div class="form_body">
+
+        <div class="form_field_container">
+
+          <button type="submit" name="schedule_apt_btn">Schedule appointments</button>
+
+          <button type="submit" name="view_apt_btn">View appointments</button>
+
+          <button type="submit" name="update_info_btn">Update information</button>
+
+        </div>
+      </div>
+    </form>
+  </div>
+</body>
+
+</html>
